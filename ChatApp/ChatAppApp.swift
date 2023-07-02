c24c79a717b539106061abe37166933802bfb28f
jdkingsbury <jerry.daniel.kingsbury@gmail.com>
2023-07-02 00:59:18 -0500

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Jerry Kingsbury on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
