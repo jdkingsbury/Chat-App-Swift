80135a2b95ec99b170809783008c01b873ae4804
jdkingsbury <jerry.daniel.kingsbury@gmail.com>
2023-08-08 03:39:51 -0500

    up to date with refactor

--- ChatApp/Service/MessageService.swift
+++ ChatApp/Service/MessageService.swift
@@ -5,59 +5,18 @@
 //  Created by Jerry Kingsbury on 7/13/23.
 //
 
-import Foundation
 import Firebase
 
 struct MessageService {
-    
-    static let messagesCollection = Firestore.firestore().collection("messages")
-    
-    static func sendMessage(_ mesageText: String, toUser user: User) {
-        guard let currentUid = Auth.auth().currentUser?.uid else { return }
-        let chatPartnerId = user.id
+    static func updateMessageStatusIfNecessary(_ messages: [Message]) async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        guard let lastMessage = messages.last, !lastMessage.read else { return }
         
-        let currentUserRef = messagesCollection.document(currentUid).collection(chatPartnerId).document()
-        let chatPartnerRef = messagesCollection.document(chatPartnerId).collection(currentUid)
-        
-        let recentCurrentRef = COLLECTION_MESSAGES.document(currentUid).collection("recent-messages").document(chatPartnerId)
-        let recentPartnerRef = COLLECTION_MESSAGES.document(chatPartnerId).collection("recent-messages").document(currentUid)
-        
-        let messageId = currentUserRef.documentID
-        
-        let message = Message(messageId: messageId,
-                              fromId: currentUid,
-                              toId: chatPartnerId,
-                              read: false,
-                              messageText: mesageText,
-                              timestamp: Timestamp())
-        
-        guard let messageData = try? Firestore.Encoder().encode(message) else { return }
-        
-        currentUserRef.setData(messageData)
-        chatPartnerRef.document(messageId).setData(messageData)
-        
-        recentCurrentRef.setData(messageData)
-        recentPartnerRef.setData(messageData)
-    }
-    
-    static func observeMessages(chatPartner: User ,completion: @escaping([Message]) -> Void) {
-        guard let currentUid = Auth.auth().currentUser?.uid else { return }
-        let chatPartnerId = chatPartner.id
-        
-        let query = messagesCollection
-            .document(currentUid)
-            .collection(chatPartnerId)
-            .order(by: "timestamp", descending: false)
-        
-        query.addSnapshotListener { snapshot, _ in
-            guard let changes = snapshot?.documentChanges.filter({ $0.type == .added }) else { return }
-            var messages = changes.compactMap({ try? $0.document.data(as: Message.self) })
-            
-            for (index, message) in messages.enumerated() where message.fromId != currentUid {
-                messages[index].user = chatPartner
-            }
-            
-            completion(messages)
-        }
+        try await COLLECTION_MESSAGES
+            .document(uid)
+            .collection("recent-messages")
+            .document(lastMessage.id)
+            .updateData(["read": true])
     }
 }
+
